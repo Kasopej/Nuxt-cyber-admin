22abd60d452dfc356f487219f0277a4c56853388
jediiry <jediiry@gmail.com>
2021-01-22 18:27:55 +0100

    update home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. At quas inventore
+    adipisci eius sed. Maxime, consequatur optio sequi iusto reprehenderit,
+    repellendus harum aspernatur autem voluptatibus minus praesentium. Dolorum,
+    libero quas?
   </div>
 </template>
 
@@ -12,6 +15,6 @@ export default {
   name: "Home",
   components: {
     // HelloWorld
-  }
+  },
 };
 </script>
