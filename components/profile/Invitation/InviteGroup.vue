4d9d043e648d91f95253809429a6b6ba3d712716
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-04-15 20:04:29 +0100

    Added Dummy group data

--- components/profile/Invitation/InviteGroup.vue
+++ components/profile/Invitation/InviteGroup.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>You have no group yet</div>
+  <div>
+    <!-- <div>You have no group yet</div> -->
+
+    <v-simple-table>
+      <template #default>
+        <!-- <thead>
+          <tr>
+            <th></th>
+            <th class="subtitle-1 ">E-mail</th>
+            <th class="subtitle-1 ">Status</th>
+            <th class="subtitle-1 ">Points</th>
+          </tr>
+        </thead> -->
+        <tbody>
+          <tr v-for="q in 5" :key="q">
+            <td><v-checkbox /></td>
+            <td>Olajide.{{ q }}.Hammed@gmail.com</td>
+            <td>Verified</td>
+            <td>{{ q }}0</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
 </template>
