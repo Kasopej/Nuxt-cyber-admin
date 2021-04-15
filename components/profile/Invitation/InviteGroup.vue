db32e45d6e31c2208638ea322da1c99996556d8e
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-04-15 20:30:33 +0100

    Update:

--- components/profile/Invitation/InviteGroup.vue
+++ components/profile/Invitation/InviteGroup.vue
@@ -4,20 +4,12 @@
 
     <v-simple-table>
       <template #default>
-        <!-- <thead>
-          <tr>
-            <th></th>
-            <th class="subtitle-1 ">E-mail</th>
-            <th class="subtitle-1 ">Status</th>
-            <th class="subtitle-1 ">Points</th>
-          </tr>
-        </thead> -->
         <tbody>
           <tr v-for="q in 5" :key="q">
-            <td><v-checkbox /></td>
             <td>Olajide.{{ q }}.Hammed@gmail.com</td>
             <td>Verified</td>
             <td>{{ q }}0</td>
+            <td><v-checkbox /></td>
           </tr>
         </tbody>
       </template>
