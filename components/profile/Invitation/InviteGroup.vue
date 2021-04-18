e25bf51c3f46ced641f06f3097dab5c3d3794b78
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-04-18 17:19:42 +0100

    Update:

--- components/profile/Invitation/InviteGroup.vue
+++ components/profile/Invitation/InviteGroup.vue
@@ -8,8 +8,12 @@
           <tr v-for="q in 5" :key="q">
             <td>Olajide.{{ q }}.Hammed@gmail.com</td>
             <td>Verified</td>
-            <td>{{ q }}0</td>
-            <td><v-checkbox /></td>
+            <td>{{ q }}0 Points</td>
+            <td>
+              <v-btn small color="accent" class="text-capitalize mr-4"
+                >Invite<v-icon small class="ml-3">mdi-email-send</v-icon></v-btn
+              >
+            </td>
           </tr>
         </tbody>
       </template>
