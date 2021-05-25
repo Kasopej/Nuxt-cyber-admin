ca170f1f73474ce63cb43e5cc333c418fc35a7d0
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-05-25 23:17:33 +0100

    Update:

--- pages/index.vue
+++ pages/index.vue
@@ -35,73 +35,9 @@
 
           <template v-else>
             <template v-if="programs.length">
-              <v-card
-                v-for="program in programs"
-                :key="program.id"
-                class="d-flex align-center mt-6 overflow-x-hidden"
-                @click="$router.push(`/program/${program._id}/`)"
-              >
-                <v-img
-                  src="/images/dummy.jpg"
-                  width="250"
-                  height="200"
-                  :max-width="$vuetify.breakpoint.mobile ? '100px' : '250px'"
-                  cover
-                />
-
-                <section
-                  style="min-height: 200px"
-                  class="
-                    flex-grow-1
-                    fill-height
-                    w-full
-                    d-flex
-                    flex-column
-                    justify-space-between
-                    pa-2 pa-md-4
-                  "
-                >
-                  <div class="d-sm-flex justify-space-between">
-                    <header
-                      class="headline font-weight-medium accent--text"
-                      v-text="program.title"
-                    />
-
-                    <aside
-                      class="
-                        grey--text
-                        text--darken-2
-                        font-weight-bold
-                        py-2
-                        px-sm-4
-                        py-sm-0
-                      "
-                      v-text="program.status"
-                    />
-                  </div>
-
-                  <div
-                    class="
-                      d-none d-sm-inline
-                      grey--text
-                      text--darken-2 text-no-wrap
-                      py-3
-                    "
-                  >
-                    <v-icon small class="mr-2">mdi-gift</v-icon>
-                    <span v-text="program.reward" />
-                  </div>
-
-                  <div class="pb-1 text-no-wrap">
-                    <v-icon small class="mr-2">mdi-counter</v-icon
-                    ><strong v-text="program.submission || 0" />
-                    Submissions
-                  </div>
-                  <div class="font-italic text-no-wrap">
-                    <small v-text="programTypes[program.type]" />
-                  </div>
-                </section>
-              </v-card>
+              <template v-for="program in programs">
+                <program-item-list :key="program.id" :program="program" />
+              </template>
             </template>
 
             <template v-else>
@@ -163,31 +99,6 @@ export default {
             : 'Something occured. Please try again',
         })
       })
-  },
-
-  methods: {
-    getProgramName(programType) {
-      let result
-      switch (programType) {
-        case 'Compliance"':
-          result = 'Vulnerability Disclosure Program'
-          break
-
-        case 'Bug Bounty':
-          result = 'Vulnerability Disclosure Program'
-          break
-
-        case 'vdp':
-          result = 'Vulnerability Disclosure Program'
-          break
-
-        default:
-          result = 'Unknown'
-          break
-      }
-
-      return result
-    },
   },
 }
 </script>
