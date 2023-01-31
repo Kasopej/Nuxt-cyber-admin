d889e59bdc0caf14137970896bfdc17e712978b6
Kasope Johnson <kasopej@gmail.com>
2023-01-31 09:24:51 +0100

    fix: removes filter & sort components from programs view as only 3 programs can be created

--- pages/index.vue
+++ pages/index.vue
@@ -7,63 +7,14 @@
     "
   >
     <v-row>
-      <!-- Filter menu bar - smaller screens version -->
-      <div v-if="mobileView">
-        <v-overlay :value="overlay">
-          <v-row>
-            <v-col cols="12" class="d-flex">
-              <PartialsFilterMenuBar
-                v-model="filterOptions"
-                @toggle-overlay="overlay = !overlay"
-              ></PartialsFilterMenuBar>
-            </v-col>
-          </v-row>
-        </v-overlay>
-      </div>
-      <!-- Filter menu bar - larger screens version -->
-      <v-col v-else cols="2" class="d-flex">
-        <PartialsFilterMenuBar v-model="filterOptions"></PartialsFilterMenuBar>
-      </v-col>
-
-      <!-- Main section containing programs list, search & sort -->
-      <v-col :cols="mobileView ? 12 : 10" class="px-4">
+      <!-- Main section containing programs list -->
+      <v-col cols="12" class="px-4">
         <v-container class="white rounded-lg pa-2 pa-md-8">
-          <!-- Search -->
-          <header class="d-flex mb-0 mb-md-6">
-            <v-col cols="12">
-              <div class="mx-auto flex justify-center text-center">
-                <input
-                  type="search"
-                  class="py-4 px-6 bg-primary text-white search-bar border-solid border rounded-lg border-primary placeholder:text-white"
-                  style="width: 70%"
-                  placeholder="Search programs"
-                />
-                <v-icon class="relative right-12 cursor" color="white" x-large
-                  >mdi-magnify</v-icon
-                >
-              </div>
-            </v-col>
-          </header>
-
           <!-- Sort & Program count -->
           <div class="d-flex px-2">
             <p class="inline-block col-6">
               {{ programs.length }} programs found
-              <v-icon v-if="mobileView" @click="overlay = !overlay"
-                >mdi-filter</v-icon
-              >
             </p>
-            <v-select
-              id="v-select-sorting"
-              v-model="sortBy"
-              :items="['newest', 'oldest']"
-              class="fit-content ml-auto mt-2 col-6"
-              dense
-            >
-              <template #prepend>
-                <v-icon @click="clickSelect">mdi-sort</v-icon>
-              </template>
-            </v-select>
           </div>
 
           <!-- Programs List -->
@@ -106,14 +57,6 @@
               <partials-empty-data v-else caption="No Programs Available" />
             </v-row>
           </main>
-
-          <!-- Pagination control -->
-          <partials-pagination
-            v-model="pagination.page"
-            :length="pagination.length"
-            :page-limit="pageLimit"
-            @input="getPrograms"
-          ></partials-pagination>
         </v-container>
       </v-col>
     </v-row>
