89d41ca05bf08319e17faf0e9cc21aa15a7476f9
Kasope Johnson <kasopej@gmail.com>
2023-02-03 17:24:09 +0100

    fix: updates submission-comment-tab SFC to match

--- components/submission/CommentTab.vue
+++ components/submission/CommentTab.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-2 py-6">
+  <main class="px-3 py-6">
     <section v-if="$fetchState.pending">
       <v-skeleton-loader v-for="i in 3" :key="i" type="article" />
     </section>
@@ -13,129 +13,142 @@
     </section>
 
     <section v-else>
-      <div
-        v-for="comment in comments"
-        :key="comment._id"
-        class="comment d-flex"
-      >
-        <v-avatar color="secondary" size="40" class="avatar mt-1 mr-2">
-          <v-img v-if="comment.hunterId" contain src="" alt="Profile" />
-          <v-icon v-else color="primary" size="39">
-            {{
-              comment.accountType === 'company'
-                ? 'mdi-factory'
-                : 'mdi-account-circle'
-            }}
-          </v-icon>
-        </v-avatar>
+      <v-timeline dense align-top>
+        <v-timeline-item v-for="comment in comments" :key="comment._id">
+          <template #icon>
+            <v-avatar>
+              <img
+                :src="
+                  comment.hunterId
+                    ? comment.hunterId.profile?.[0]?.image ??
+                      '/images/dummy.jpg'
+                    : comment.accountId.company?.[0]?.image ??
+                      '/images/dummy.jpg'
+                "
+              />
+            </v-avatar>
+          </template>
+          <v-card>
+            <v-card-title
+              class="flex px-3 py-2 text-body-2 secondary lighten-8"
+            >
+              <v-tooltip top nudge-bottom="20" class="px-1 py-1">
+                <template #activator="{ on, attrs }">
+                  <span v-bind="attrs" v-on="on">{{
+                    comment.hunterId
+                      ? comment.hunterId.profile?.[0].username
+                      : comment.accountId.company?.[0].name
+                  }}</span>
+                </template>
+                <span class="text-xs">100 points</span>
+              </v-tooltip>
+              <!-- <span>{{
+                comment.hunterId
+                  ? comment.hunterId.profile[0].username
+                  : comment.accountId.company[0].name
+              }}</span> -->
+              <v-chip dense class="ml-1 px-2 py-1" small>{{
+                comment.accountType
+              }}</v-chip>
+              <div class="ml-auto fit-content flex">
+                Commented on
+                {{ new Date(comment.createdAt).toDateString() }}
+                <v-icon color="primary" class="-mt-0.5 ml-1">{{
+                  visibilityIcon(comment)
+                }}</v-icon>
+              </div>
+            </v-card-title>
+            <v-card-text class="px-3 py-3">
+              <article v-html="convertCommentHTML(comment.comment)"></article>
+            </v-card-text>
+          </v-card>
+        </v-timeline-item>
+      </v-timeline>
+    </section>
 
-        <v-card class="flex-grow-1 pa-3" elevation="0" outlined>
-          <header class="d-sm-flex justify-space-between flex-grow-1">
-            <div class="comment__info body-2 text-small">
-              <a href="http://" target="_blank" rel="noopener noreferrer"
-                >---</a
+    <!-- <div class="text-center mt-8">
+      <v-btn
+        v-if="pagination.page < pagination.length"
+        color="primary"
+        elevation="2"
+        :loading="loadingMore"
+        small
+        rounded
+        @click="loadMoreComments"
+      >
+        Add More
+      </v-btn>
+    </div> -->
+
+    <!-- <p v-if="loadingMore" class="text-end mt-4">Adding to list...</p> -->
+
+    <v-form ref="commentForm">
+      <div class="accent--text headline font-weight-bold py-4">
+        Post A Response
+      </div>
+
+      <v-card class="pa-4" elevation="3">
+        <div>
+          <v-btn
+            small
+            :outlined="!!commentPreview"
+            color="accent"
+            @click="commentPreview = null"
+            >Write
+            <v-icon small class="ml-2">mdi-fountain-pen-tip</v-icon></v-btn
+          >
+          <v-btn
+            small
+            :outlined="!commentPreview"
+            color="accent"
+            class="my-2 mx-md-3"
+            @click="commentPreview = convertCommentHTML(FORM.comment)"
+            >Preview <v-icon small class="ml-2">mdi-eye</v-icon></v-btn
+          >
+        </div>
+        <article class="flex">
+          <v-avatar size="50" class="col-1 py-0 px-0">
+            <img src="/img/dummy.jpg" alt="user profile image" />
+          </v-avatar>
+          <div class="col-11 py-0 px-1">
+            <div
+              v-if="commentPreview"
+              class="elevation-2 relative comment-preview rounded px-2 py-4"
+              v-html="commentPreview"
+            />
+            <v-textarea
+              v-else
+              v-model="FORM.comment"
+              else
+              outlined
+              hide-details
+            />
+
+            <div class="pb-4">
+              <small class="grey--text darken-2"
+                >Styling with MarkDown is supported</small
               >
             </div>
 
-            <time class="grey--text body-2 text-small">
-              <span>({{ new Date(comment.createdAt).toLocaleString() }})</span>
-            </time>
-          </header>
-
-          <article
-            class="py-4 body-1 text-medium"
-            v-html="convertCommentHTML(comment.comment)"
-          ></article>
-
-          <footer class="text-right">
-            <span
-              class="accent--text text-caption font-weight-medium"
-              v-text="comment.status"
-            ></span>
-            <v-icon small class="ml-2" color="accent">{{
-              comment.type == 'Public' ? 'mdi-eye' : 'mdi-eye-off'
-            }}</v-icon>
-          </footer>
-        </v-card>
-      </div>
-    </section>
-
-    <v-form ref="commentForm" class="py-8">
-      <div class="accent--text headline font-weight-bold pb-4">
-        Post A Respone
-      </div>
-      <v-row>
-        <v-col>
-          <v-autocomplete
-            v-model="FORM.action"
-            dense
-            outlined
-            label="Action"
-            :items="actions"
-          >
-          </v-autocomplete>
-        </v-col>
-        <v-col>
-          <v-switch
-            v-model="FORM.private"
-            :label="FORM.private ? 'Open Report?' : 'Close Report?'"
-          ></v-switch>
-        </v-col>
-      </v-row>
-
-      <v-card class="pa-4" elevation="3">
-        <v-row>
-          <v-col>
             <v-btn
-              small
-              :outlined="!!commentPreview"
-              color="accent"
-              @click="commentPreview = null"
-              >Write
-              <v-icon small class="ml-2">mdi-fountain-pen-tip</v-icon></v-btn
-            >
-            <v-btn
-              small
-              :outlined="!commentPreview"
-              color="accent"
-              class="my-2 mx-md-3"
-              @click="previewComment()"
-              >Preview <v-icon small class="ml-2">mdi-eye</v-icon></v-btn
-            >
-          </v-col>
-          <v-col>
-            <v-autocomplete
-              v-model="selectedPresetComment"
-              dense
-              outlined
-              label="Comment Templates"
-              :items="presetComments"
-              item-value="content"
-              item-text="title"
-              @change="FORM.comment = selectedPresetComment"
-            >
-            </v-autocomplete>
-          </v-col>
-        </v-row>
-
-        <!-- eslint-disable-next-line vue/no-v-html -->
-        <div
-          v-if="commentPreview"
-          class="rounded px-2 py-4"
-          v-html="commentPreview"
-        />
-        <v-textarea v-else v-model="FORM.comment" else outlined hide-details />
-
-        <div class="pb-4">
-          <small class="grey--text darken-2"
-            >Styling with MarkDown is supported</small
-          >
-        </div>
-
-        <v-btn color="primary" @click="postComment()">Post Response</v-btn>
-      </v-card></v-form
-    >
-  </div>
+              color="primary"
+              :disabled="!FORM.comment.length || formSubmitting"
+              @click="postComment"
+              >Post Response
+              <v-progress-circular
+                v-if="formSubmitting"
+                class="ml-3"
+                :size="23"
+                :width="2"
+                indeterminate
+                color="primary"
+              ></v-progress-circular>
+            </v-btn>
+          </div>
+        </article>
+      </v-card>
+    </v-form>
+  </main>
 </template>
 
 <script>
@@ -145,7 +158,9 @@ import presetComments from '~/assets/presets/comments.json'
 export default {
   data() {
     return {
-      FORM: {},
+      FORM: {
+        comment: '',
+      },
       comments: [],
       presetComments,
 
@@ -163,6 +178,9 @@ export default {
 
       selectedPresetComment: null,
       commentPreview: null,
+      formSubmitting: false,
+      pagination: { page: 1, length: 0 },
+      loadingMore: false,
     }
   },
 
@@ -200,6 +218,9 @@ export default {
   },
 
   methods: {
+    visibilityIcon(comment) {
+      return comment.type === 'Public' ? 'mdi-eye' : 'mdi-eye-off'
+    },
     convertCommentHTML(val) {
       const converter = new showdown.Converter()
       return converter.makeHtml(val)
