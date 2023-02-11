6a5dae5cd5fb99d5f28e4377ed631dc6b2daf733
Kasope Johnson <kasopej@gmail.com>
2023-02-11 11:53:42 +0100

    fix: cleans up leftover code fetching programs in index page

--- pages/index.vue
+++ pages/index.vue
@@ -3,34 +3,8 @@
 </template>
 
 <script>
-import { createNamespacedHelpers } from 'vuex'
-const { mapMutations } = createNamespacedHelpers('program')
 export default {
   layout: 'dashboard',
   middleware: 'auth',
-  created() {
-    this.loadPrograms()
-  },
-  methods: {
-    ...mapMutations(['SAVE_DATA']),
-    async loadPrograms() {
-      const URL = `/load-programs`
-      // Make upload request to the API
-      await this.getHTTPClient()
-        .$get(URL)
-        .then((res) => {
-          this.SAVE_DATA(res.data)
-        })
-        .catch((error) => {
-          this.$store.commit('notification/SHOW', {
-            color: 'accent',
-            icon: 'mdi-alert-outline',
-            text: error.response
-              ? error.response.data.message
-              : 'Something occured. Please try again',
-          })
-        })
-    },
-  },
 }
 </script>
